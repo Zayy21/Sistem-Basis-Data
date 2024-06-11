8ed1794a746cb3bd14ccefde00df495e286c9fb3
Revan <revanhklsp@gmail.com>
2024-06-12 03:20:13 +0700

    migration fixes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,21 +14,14 @@ return new class extends Migration
         Schema::create('Customers', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->integer('meja');
+            $table->integer('meja')->unique();
             $table->integer('stick');
             $table->integer('gloves');
             $table->integer('jam')->unsigned();
+            $table->string('metodePembayaran');
+            $table->string('transaksi');
             $table->timestamps();
             $table->softDeletes();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
