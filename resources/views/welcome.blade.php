132d6ba127fdbe5ddf815412a6d3c6e8d745e5c1
Zayy21 <afa.fakhri@gmail.com>
2024-06-14 22:33:52 +0700

    fixTimer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,10 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
-
     <title>Mod Billiard</title>
 </head>
 <body>
@@ -26,58 +22,76 @@
     <div class="container body-table py-2">
         @if(isset($payments) && count($payments) > 0)
             @foreach($payments as $payment)
-                <div class="row text-center">
-                    <div class="col-2 p-0"><div class="text-body" style="border-left-style: solid; border-top-left-radius: 10px; border-bottom-left-radius: 10px;"><p>{{ $payment->meja }}</p></div></div>
-                    <div class="col-5 p-0"><div class="text-body" id="nama"><p>{{ $payment->nama }}</p></div></div>
-                    <div class="col-2 p-0"><div class="text-body"><p>{{ $payment->stick }}</p></div></div>
-                    <div class="col-3 p-0"><div class="text-body" style="border-right-style: solid; border-top-right-radius: 10px; border-bottom-right-radius: 10px;"><p>{{ gmdate("H:i:s", $payment->jam * 3600) }}</p></div></div>
+                <div class="row text-center payment-row" id="payment-row-{{ $payment->id }}">
+                    <div class="col-2 p-0">
+                        <div class="text-body" style="border-left-style: solid; border-top-left-radius: 10px; border-bottom-left-radius: 10px;">
+                            <p>{{ $payment->meja }}</p>
+                        </div>
+                    </div>
+                    <div class="col-5 p-0">
+                        <div class="text-body" id="nama">
+                            <p>{{ $payment->nama }}</p>
+                        </div>
+                    </div>
+                    <div class="col-2 p-0">
+                        <div class="text-body">
+                            <p>{{ $payment->stick }}</p>
+                        </div>
+                    </div>
+                    <div class="col-3 p-0">
+                        <div class="text-body countdown-timer" data-hours="{{ $payment->jam }}" style="border-right-style: solid; border-top-right-radius: 10px; border-bottom-right-radius: 10px;">
+                            <p class="time-remaining" id="time-remaining-{{ $payment->id }}"></p>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         @else
             <div class="row text-center">
-                <div class="col-12 p-0"><div class="text-body"><p>No payments found.</p></div></div>
+                <div class="col-12 p-0">
+                    <div class="text-body"><p>No payments found.</p></div>
+                </div>
             </div>
         @endif
     </div>
 <!-- body table end -->
 <!-- sum stick start -->
-    <!-- sum stick end -->
+<!-- sum stick end -->
 <!-- sum gloves start -->
-    <!-- sum gloves end -->
+<!-- sum gloves end -->
 <!-- pick table start -->
     <div class="col booking p-0">
         <div class="container booking-meja">
             <div class="row align-items-start">
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(1)" data-table="1">1</button>
+                    <button class='meja' onclick="selectTable(1)" data-table="1">1</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(2)" data-table="2">2</button>
+                    <button class='meja' onclick="selectTable(2)" data-table="2">2</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(3)" data-table="3">3</button>
+                    <button class='meja' onclick="selectTable(3)" data-table="3">3</button>
                 </div>
             </div>
             <div class="row align-items-center">
-            <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(4)" data-table="4">4</button>
+                <div class="col p-0 my-2 mx-1">
+                    <button class='meja' onclick="selectTable(4)" data-table="4">4</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(5)" data-table="5">5</button>
+                    <button class='meja' onclick="selectTable(5)" data-table="5">5</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(6)" data-table="6">6</button>
+                    <button class='meja' onclick="selectTable(6)" data-table="6">6</button>
                 </div>
             </div>
             <div class="row align-items-end">
-            <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(7)" data-table="7">7</button>
+                <div class="col p-0 my-2 mx-1">
+                    <button class='meja' onclick="selectTable(7)" data-table="7">7</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(8)" data-table="8">8</button>
+                    <button class='meja' onclick="selectTable(8)" data-table="8">8</button>
                 </div>
                 <div class="col p-0 my-2 mx-1">
-                  <button class='meja' onclick="selectTable(9)" data-table="9">9</button>
+                    <button class='meja' onclick="selectTable(9)" data-table="9">9</button>
                 </div>
             </div>
             <div class="row align-items-end">
@@ -88,12 +102,9 @@
         </div>
     </div>
 <!-- pick table end -->
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <script>
+<!-- Optional JavaScript; choose one of the two! -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script>
     function selectTable(tableNumber) {
         sessionStorage.setItem('selectedTable', tableNumber);
         const lanjutButton = document.getElementById('lanjutButton');
@@ -101,10 +112,41 @@
         const selectedButton = document.querySelector(`.meja[data-table="${tableNumber}"]`);
         selectedButton.classList.add('pressed');
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const countdownElements = document.querySelectorAll('.countdown-timer');
+
+        countdownElements.forEach(function(element) {
+            const hours = element.getAttribute('data-hours');
+            const timerId = element.querySelector('.time-remaining').id;
+            const localStorageKey = `countdown-${timerId}`;
+
+            let deadline = localStorage.getItem(localStorageKey);
+
+            if (!deadline) {
+                deadline = new Date(new Date().getTime() + hours * 3600 * 1000);
+                localStorage.setItem(localStorageKey, deadline);
+            } else {
+                deadline = new Date(deadline);
+            }
+
+            const interval = setInterval(function() {
+                const now = new Date().getTime();
+                const t = deadline - now;
+                const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+                document.getElementById(timerId).innerHTML = `${hours}:${minutes}:${seconds}`;
+
+                if (t < 0) {
+                    clearInterval(interval);
+                    document.getElementById(timerId).innerHTML = "00:00:00";
+                    document.getElementById(`payment-row-${element.dataset.id}`).remove();
+                }
+            }, 1000);
+        });
+    });
 </script>
-
-
-    
-
 </body>
 </html>
